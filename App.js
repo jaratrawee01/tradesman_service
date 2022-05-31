b6c67f1a61081ff85df97ff70c4b6b8294f6fc22
THANETNove <Novel_Manga@outlook.co.th>
2022-06-01 04:43:02 +0700

    up navbar ใหม่

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,View} from 'react-native';
 import  Login  from './screens/login';
 import  Test_icon  from './screens/test_icon';
+import  Nave  from './screens/navber';
 
 export default function App() {
   return (
     <View>
-  <Login />
+ {/*  <Login /> */}
   {/*   <Test_icon />   */}
+     <Nave />   
     {/* ส่วนหัว  */}
   { <StatusBar style="auto" /> }
    </View>
