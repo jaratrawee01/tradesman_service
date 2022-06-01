19ad1a3c3a6170756e9fb9ee67bbcb7e26e0e389
jaratrawee01 <appyer01@gmail.com>
2022-06-02 01:45:38 +0700

    ปุมnav

--- screens/navbr2.js
+++ screens/navbr2.js
@@ -16,25 +16,25 @@ export default function App() {
             <View style={styles.footer}>
                 <View style={styles.navbar}>
 
-                    <Text style={styles.textbox1}>
-                        <MaterialCommunityIcons name="home" size={35} color="#01C1FF" />
-                        <Text>Home</Text>
-                    </Text>
+                    <View style={styles.textbox1}>
+                        <MaterialCommunityIcons name="home" style={styles.iconColor} />
+                        <Text style={styles.textIcon}>Home</Text>
+                    </View>
 
-                    <Text style={styles.textbox1}>
-                        <MaterialCommunityIcons name="shopping" size={35} color="#01C1FF" />
-                        <Text style={styles.text}>Shop</Text>
-                    </Text>
+                    <View style={styles.textbox1}>
+                        <MaterialCommunityIcons name="shopping" style={styles.iconColor} />
+                        <Text style={styles.textIcon}>Shop</Text>
+                    </View>
 
-                    <Text style={styles.textbox1}>
-                        <MaterialCommunityIcons name="message-processing" size={35} color="#01C1FF" />
-                        <Text>Chat</Text>
-                    </Text>
+                    <View style={styles.textbox1}>
+                        <MaterialCommunityIcons name="message-processing" style={styles.iconColor} />
+                        <Text style={styles.textIcon}>Message</Text>
+                    </View>
 
-                    <Text style={styles.textbox1}>
-                        <FontAwesome name="user" size={35} color="#01C1FF" />
-                        <Text>Profile</Text>
-                    </Text>
+                    <View style={styles.textbox1}>
+                        <FontAwesome name="user" style={styles.iconColor} />
+                        <Text style={styles.textIcon}>Profile</Text>
+                    </View>
                     
                 </View>
             </View>
@@ -59,35 +59,34 @@ const styles = StyleSheet.create({
     },
     navbar: {
         width: '100%',
-        height: 90,
+        height: 70,
         flexDirection: 'row',
         shadowColor: '#000',
         shadowOpacity: 0.1,
         shadowRadius: 10,
         elevation: 2,
+        paddingLeft: 5,
+        paddingRight: 5,
 
     },
     footer: {
         flex: 1,
         justifyContent: 'flex-end',
+        marginLeft: -5,
+        marginRight: -5,
     },
     textbox1: {
         position: 'relative',
-        marginTop: 15,
-        marginLeft: 30,
-        margin: 10,
-        width: 50, 
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        height: 60, 
     },
-    text: {
-         flex: 1,
-         justifyContent: 'center',
-         alignItems: 'center',
-         position: 'absolute',
-         marginLeft: 50,
+    textIcon: {
+      fontSize: 16,
+    },
+    iconColor: {
+        fontSize: 34, 
+        color: '#01C1FF',
     },
 
 });
