ca949bd99c7df8c14b3e418177dbcb968ade309c
jaratrawee01 <appyer01@gmail.com>
2022-06-10 03:17:38 +0700

    แก้ไข

--- screens/navbar/profile_user.js
+++ screens/navbar/profile_user.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, Text,  View, Image, ScrollView } from 'react-native';
+import { SafeAreaView, StyleSheet, Text,  View, ImageBackground, Image, ScrollView } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 
+
+const image = { uri: 'https://www.roojai.com/wp-content/uploads/2018/07/how-to-choose-garage-car-mechanic-cover.jpg' };
 
 class Profile_user extends Component {
   render() {
@@ -11,8 +13,49 @@ class Profile_user extends Component {
 
       <SafeAreaView style={styles.container}>
         <ScrollView>
+          <View>
+            <ImageBackground source={image} resizeMode="cover" style={styles.backgroun}>
+                <Image
+                  style={styles.image}
+                  source={{uri: 'https://www.cdti.ac.th/uploads/images/image_750x422_5da3c6560cde8.jpg'}}
+                />
+                <Text style={styles.text}>Yonzook</Text>
+                <View style={styles.icons1}>
+                  <View style={styles.boxhead}>
+                      <FontAwesome name="star" style={styles.icons} />
+                      <FontAwesome name="star" style={styles.icons} />
+                      <FontAwesome name="star" style={styles.icons} />
+                      <FontAwesome name="star" style={styles.icons} />
+                      <FontAwesome name="star" style={styles.icons} />
+                  </View>
+                </View>
+            </ImageBackground>
+          </View>
+
           <View style={styles.box1}>
-            
+              <View style={styles.box2}>
+
+                  <View style={styles.boxhead}>
+                    <FontAwesome5 name="user-alt" style={styles.icons2} />
+                    <Text style={styles.text1}>{'Profile'}</Text>
+                  </View> 
+
+                  <View style={styles.boxhead}>
+                    <FontAwesome5 name="address-book" style={styles.icons2} />
+                    <Text style={styles.text1}>{'Contact informatiom'}</Text>  
+                  </View>  
+
+                  <View style={styles.boxhead}>
+                    <FontAwesome5 name="unlock-alt" style={styles.icons2} />
+                    <Text style={styles.text1}>{'Change user/password'}</Text>   
+                  </View> 
+
+                  <View style={styles.boxhead}>
+                    <Ionicons name="chatbox-ellipses-sharp" style={styles.icons2} />
+                    <Text style={styles.text1} onPress={() => this.props.navigation.navigate('Login')}>{'Cnat'}</Text>
+                  </View> 
+
+              </View>
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -29,105 +72,72 @@ const styles = StyleSheet.create({
     position: 'relative',
     backgroundColor: '#f8fdff',
   },
+  backgroun: {
+    width: 360,
+    height: 250 ,
+  },
+  boxhead: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
   image: {
     width: 100,
     height: 100,
-    marginTop: 20,
-    borderRadius: 15,
+    marginTop: 40,
+    borderRadius: 50,
     marginLeft: 'auto',
     marginRight: 'auto',
   },
   box1: {
-    height: 250,
-    width: 300,
-    backgroundColor: '#fff',
-    shadowColor: "#000",
-    shadowOpacity: 0.27,
-    shadowRadius: 4.65,
-    elevation: 6,
-    borderRadius: 20,
-    marginLeft: 28,
-    marginRight: 20,
-    marginTop: 25,
+    height: 365,
+    width: 360,
+    backgroundColor: '#FFF0F0',    
   },
-  box3: {
-    height: 60,
-    width: 300,
-    backgroundColor: '#fff',
+  box2: {
+    height: 300,
+    width: 320,
+    backgroundColor: '#f8fdff',
     shadowColor: "#000",
-    shadowOpacity: 0.27,
-    shadowRadius: 4.65,
-    elevation: 3,
-    borderRadius: 10,
-    marginLeft: 28,
+    shadowOpacity: 0.39,
+    shadowRadius: 8.30,
+    elevation: 13,
+    borderRadius: 15,
+    marginLeft: 20,
     marginRight: 20,
-    marginTop: 15,
+    marginTop: -20,
     marginBottom: 4,
   },
   text: {
     marginLeft: 'auto',
     marginRight: 'auto',
     fontWeight: 'bold',
-    fontSize: 25,
-    marginTop: 10,
-  },
-  text1: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    fontSize: 20,
+    fontSize: 30,
+    color: '#00c2fe',
     marginTop: 5,
   },
-  text2: {
-    marginLeft: 80,
-    fontSize: 20,
-    marginTop: -35,
+  text1: {
+    marginLeft: 20,
+    fontSize: 24,
+    marginTop: 30,
   },
   icons: {
-    marginLeft: 80,
-    marginTop: 15,
+    marginLeft: 5,
     fontSize: 24, 
-    color: '#bcbdbe'
+    color: '#d7d13d'
   },
   icons1: {
-    marginLeft: 110,
-    marginTop: 15,
-    fontSize: 24, 
-    color: '#bcbdbe',
-    marginTop: -25,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 10,
   },
   icons2: {
-    marginLeft: 140,
-    marginTop: 15,
-    fontSize: 24, 
-    color: '#bcbdbe',
-    marginTop: -25,
-  },
-  icons3: {
-    marginLeft: 170,
-    marginTop: 15,
-    fontSize: 24, 
-    color: '#bcbdbe',
-    marginTop: -25,
-  },
-  icons4: {
-    marginLeft: 200,
-    marginTop: 15,
-    fontSize: 24, 
-    color: '#bcbdbe',
-    marginTop: -25,
-  },
-  icons5: {
-    width: 50,
-    height: 48,
-    marginLeft: 10,
-    marginTop: 6,
-    fontSize: 24, 
+    marginTop: 30,
+    marginLeft: 25,
+    fontSize: 26,
     color: '#00c2fe',
-    backgroundColor: '#dcecf4',
-    borderRadius: 10,
-    paddingTop: 12,
-    paddingLeft: 14,
   },
+
+ 
 });
 
 
@@ -135,36 +145,3 @@ const styles = StyleSheet.create({
 export default Profile_user; 
 
 
-{/* <Image
-                style={styles.image}
-                source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI1zrmmzTYC6hHZP-XapM5K0eCU3cy8LldkTz6C-MXkUxO6iRX1GQvar3LCvtzvhzWb2Y&usqp=CAU'}}
-             />
-             <Text style={styles.text}>Jason Amada</Text>
-             <Text style={styles.text1}>aaaa_dddd@gmail.com</Text>
-             <FontAwesome name="star" style={styles.icons} />
-             <FontAwesome name="star" style={styles.icons1} />
-             <FontAwesome name="star" style={styles.icons2} />
-             <FontAwesome name="star" style={styles.icons3} />
-             <FontAwesome name="star" style={styles.icons4} />
-          </View>
-
-          <View style={styles.box3}>
-              <FontAwesome5 name="user-alt" style={styles.icons5} />
-              <Text style={styles.text2}>{'Profile'}</Text>                            
-          </View>
-
-          <View style={styles.box3}>
-              <FontAwesome5 name="address-book" style={styles.icons5} />
-              <Text style={styles.text2}>{'Contact informatiom'}</Text>                           
-          </View>
-
-          <View style={styles.box3}>
-              <FontAwesome5 name="unlock-alt" style={styles.icons5} />
-              <Text style={styles.text2}>{'Change user/password'}</Text>                             
-          </View>
-
-          <View style={styles.box3}>
-              <Ionicons name="chatbox-ellipses-sharp" style={styles.icons5} />
-              <Text style={styles.text2} onPress={() => this.props.navigation.navigate('Login')}>{'Cnat'}</Text>                             
-          </View>
- */}
