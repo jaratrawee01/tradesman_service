80074fbcd4501e5d3991d17e9720cac5f19d0204
jaratrawee01 <appyer01@gmail.com>
2022-07-09 18:04:33 +0700

    lohin

--- screens/login.js
+++ screens/login.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, Text, TextInput, Image, View, TouchableOpacity, ScrollView } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import React, { Component } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  Image,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import login from "./service/getService";
 
-
 class Login extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -15,129 +22,144 @@ class Login extends Component {
     };
   }
 
-  handleOnPress(e,user) {
+  handleOnPress(e, user) {
     this.setState({
-      [user]: e
-    })
+      [user]: e,
+    });
   }
-   login =  async () => {
+  login = async () => {
     const data = [username];
     const getLogin = await login.getLogin(data);
-    console.log("getLogin",getLogin);
-  }
+    console.log("getLogin", getLogin);
+  };
 
   render() {
-
-    console.log(this.state.username,this.state.password);
+    console.log(this.state.username, this.state.password);
     return (
       <SafeAreaView style={styles.container}>
         <ScrollView>
-            <View style={styles.box}>
-                <Image
-                  style={styles.image2}
-                  source={require('../assets/images/A-11.png')}
+          <View>
+            <Image
+              style={styles.image2}
+              source={require("../assets/images/A-3.png")}
+            />
+          </View>
+
+          <View style={styles.box}>
+            <View style={styles.marginTop}>
+              <View style={styles.input}>
+                <FontAwesome name="user" style={styles.icons3} />
+                <TextInput
+                  style={{ flex: 1, paddingLeft: 12, fontSize: 20 }}
+                  placeholder="Name"
+                  underlineColorAndroid="transparent"
+                  onChange={(e) =>
+                    this.handleOnPress(e.nativeEvent.text, "username")
+                  }
                 />
               </View>
 
-
-          <View style={styles.marginTop}>
-            <View style={styles.input}>
-              <FontAwesome name="user" style={styles.icons3} />
-              <TextInput
-                style={{ flex: 1, paddingLeft: 12 , fontSize: 20 }}
-                placeholder="Name"
-                underlineColorAndroid="transparent" onChange={(e)=> this.handleOnPress(e.nativeEvent.text,'username')}
-              />
+              <View style={styles.input}>
+                <FontAwesome name="unlock-alt" style={styles.icons3} />
+                <TextInput
+                  style={{ flex: 1, paddingLeft: 12, fontSize: 20 }}
+                  placeholder="Password"
+                  underlineColorAndroid="transparent"
+                  onChange={(e) =>
+                    this.handleOnPress(e.nativeEvent.text, "password")
+                  }
+                />
+              </View>
             </View>
 
-            <View style={styles.input}>
-              <FontAwesome name="unlock-alt" style={styles.icons3} />
-              <TextInput
-                style={{ flex: 1, paddingLeft: 12 , fontSize: 20}}
-                placeholder="Password"
-                underlineColorAndroid="transparent"
-                onChange={(e)=> this.handleOnPress(e.nativeEvent.text,"password")}
-              />
-            </View>
-          </View>
-
-          <View style={styles.icons}>
-            <AntDesign name="checkcircleo" size={20} color="#37C1FB" />
-            <Text style={styles.text1}>{'จดจำฉัน'}</Text>
-            <View style={styles.button2}>
-            <Text
-              style={styles.text2}
-              title="Go to Details"
-              onPress={() => this.props.navigation.navigate('Registration')}>สมักสมาชิก</Text>
-            </View>
-          </View>
-  
-          <View style={styles.button}>
-            <TouchableOpacity style={styles.button1} onPress={() => this.login()}>
-              <Text style={{ fontSize: 30, textAlign: 'center', color: 'white', marginTop: 4, }} >Log in</Text>
-            </TouchableOpacity>
-          </View>
-{/* 
-          <View style={styles.boxIcon}>
-            <View style={styles.icons1}>
-              <FontAwesome name="facebook" size={35} color="#00c2fe" />
+            <View style={styles.icons}>
+              <AntDesign name="checkcircleo" size={20} color="#37C1FB" />
+              <Text style={styles.text1}>{"จดจำฉัน"}</Text>
+              <View style={styles.button2}>
+                <Text
+                  style={styles.text2}
+                  title="Go to Details"
+                  onPress={() => this.props.navigation.navigate("Registration")}
+                >
+                  สมักสมาชิก
+                </Text>
+              </View>
             </View>
 
-            <View style={styles.icons2}>
-              <AntDesign name="google" size={35} color="#00c2fe" />
+            <View style={styles.button}>
+              <TouchableOpacity
+                style={styles.button1}
+                onPress={() => this.login()}
+              >
+                <Text
+                  style={{
+                    fontSize: 30,
+                    textAlign: "center",
+                    color: "white",
+                    marginTop: 4,
+                  }}
+                >
+                  Log in
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.boxIcon}>
+              <View style={styles.icons1}>
+                <FontAwesome name="facebook" size={35} color="#00c2fe" />
+              </View>
+
+              <View style={styles.icons2}>
+                <AntDesign name="google" size={35} color="#00c2fe" />
+              </View>
             </View>
           </View>
-           */}
         </ScrollView>
       </SafeAreaView>
     );
   }
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
-    position: 'relative',
-    backgroundColor: '#fff'
+    width: "100%",
+    height: "100%",
+    position: "relative",
+    backgroundColor: "#fff",
   },
   boxIcon: {
     height: 100,
   },
   bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
+    color: "blue",
+    fontWeight: "bold",
     fontSize: 30,
   },
   box: {
-    height: 150,
-    width: 150,
-    backgroundColor: '#37C1FB',
+    height: 500,
+    width: 360,
+    backgroundColor: '#fff',
     shadowColor: "#000",
     shadowOpacity: 0.27,
     shadowRadius: 4.65,
     elevation: 6,
-    borderRadius: 100,
-    marginTop: 30,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    borderWidth: 5,
-    borderColor: '#fff',
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    marginTop: -10,
+
   },
   input: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     height: 50,
     marginLeft: 40,
     marginRight: 40,
     padding: 10,
     marginTop: 30,
     borderRadius: 30,
-    backgroundColor: '#fff',
-    shadowColor: '#000',
+    backgroundColor: "#e8e9e9",
+    shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowRadius: 10,
     elevation: 4,
@@ -145,8 +167,8 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 444,
-    textAlign: 'center',
-    fontWeight: 'bold',
+    textAlign: "center",
+    fontWeight: "bold",
     fontSize: 30,
   },
   text1: {
@@ -168,7 +190,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   button1: {
-    backgroundColor: '#37C1FB',
+    backgroundColor: "#37C1FB",
     height: 50,
     borderRadius: 25,
   },
@@ -187,48 +209,45 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 50,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowRadius: 10,
     elevation: 4,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginTop: 20,
     marginLeft: 110,
-    alignItems: 'center',
+    alignItems: "center",
     padding: 10,
   },
   icons2: {
     width: 50,
     height: 50,
     borderRadius: 50,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowRadius: 10,
     elevation: 4,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginLeft: 210,
-    alignItems: 'center',
+    alignItems: "center",
     padding: 8,
     marginTop: -50,
   },
   icons3: {
     fontSize: 24,
-    color: '#37C1FB',
+    color: "#37C1FB",
     marginLeft: 10,
   },
   image2: {
-    width: 100,
-    height: 100,
-    marginTop: 20,
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    height: 220,
+    width: 360,
+    marginLeft: "auto",
+    marginRight: "auto",
   },
   scrollView: {
     marginHorizontal: 5,
-    height: '100%',
-  }
-
+    height: "100%",
+  },
 });
-
 
 export default Login;
